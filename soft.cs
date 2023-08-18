840514976ee51d125ef0c1ff98cea106aca7a2c6
Alans Ciemgals <alan0648@mail.hansenberg.dk>
2023-08-18 13:16:09 +0200

    add ideas

--- soft.cs
+++ soft.cs
@@ -9,5 +9,9 @@ class Testy
         }
 
 }
-
+/* im still unsure how to dev this so i got a couple options
+1. simple terminal application as everyone else, maybe make fanciers with pages that clear out the screen every time and has a look trough pages thing with a list
+2. make a web app with .net so its nice and fancy
+3. make it so when launching you have the option whether to run on web or terminal
+*/
 }
