9e7f8363a589c456df31d956a5160a76285681f6
Alans Ciemgals <alan0648@mail.hansenberg.dk>
2023-08-22 14:45:07 +0200

    quick stuff

--- soft.cs
+++ soft.cs
@@ -5,7 +5,17 @@ class Testy
 {
  static void Main(string[] args)
         {
+bool run = true;
+
+
             System.Console.WriteLine("hello cock");
+            while (run == true)
+            {
+
+
+
+            }       
+
         }
 
 }
@@ -13,5 +23,7 @@ class Testy
 1. simple terminal application as everyone else, maybe make fanciers with pages that clear out the screen every time and has a look trough pages thing with a list
 2. make a web app with .net so its nice and fancy
 3. make it so when launching you have the option whether to run on web or terminal
+
+Decided to make text based very simple first in abt a hour, if i got time and willpower will make a better version after.
 */
 }
