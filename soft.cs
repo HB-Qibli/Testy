dbed0111df421bab939fe77d661ca29416ca1e78
Alans Ciemgals <alan0648@mail.hansenberg.dk>
2023-08-31 10:37:06 +0200

    added stuff

--- soft.cs
+++ soft.cs
@@ -1,21 +1,74 @@
 // Testy
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+
 namespace System
 {
 class Testy
 {
  static void Main(string[] args)
         {
+            string answer; // first few lines here are prep
+            bool run = true;
+            int typespeed = 60;
             Storagehandler handl = new Storagehandler(); //honestly unnecesary, making it static would have been just fine as multiple instances wont ever be needed
 			Displayer display = new Displayer(); // but ig to show teacher that the student is able to do this and understand what it does ig
 
 
-            Console.WriteLine("hello cock");
-            while (2+2!=5)//i'll just break out of this, only way its fucked is if some stupid ass particle travels for eons just to flip a bit
+            //do stuff here like welcome to this application and shit, maybe fancy with coloured text too
+
+            do
             {
-                
+                display.WriteLine("Select a option by number: ", typespeed);
+                display.WriteLine("1. Create a note", typespeed);
+                display.WriteLine("2. Read saved notes", typespeed);
+                display.WriteLine("3. delete notes", typespeed);
+                display.WriteLine("4. help", typespeed);
+                display.WriteLine("5. exit ", typespeed);
+                display.WriteLine("Select option: ", typespeed);
+
+                answer = Console.ReadLine();
+
+                switch (answer) 
+                {
+                    case "1":
+                    handl.CreateNote();
+                    break;
+                    
+                    case "2":
+                    handl.ViewNotesChapters();
+                    display.WriteLine("Select chapter to read (case-senstive input): ", typespeed);
+                    answer = Console.ReadLine(); // no need to make a second variable, saves mem, unless compiler re-uses a variable even when you write 2, dunno.
+                    handl.ReadNote(answer);
+                    break;
+
+                    case "3":
+                    /*
+                    TODO: include option to wipe all files with for each function later, then can add a private string as password for it and show how to safely get it
+                    */
+                    handl.ViewNotesChapters();
+                    display.WriteLine("Which chapter would you like to delete: ", typespeed); 
+                    answer = Console.ReadLine();
+                    handl.DeleteNote(answer);
+                    display.WriteLine("press enter to continue", typespeed);
+                    Console.ReadLine();
+                    break;
+
+                    case "4":
+                    //insert help stuff, like explain things
+                    break;
+
+                    case "5":
+                    run = false;
+                    break;
+                }
 
 
-            }       
+                System.Threading.Thread.Sleep(1000);
+                Console.Clear();
+            }  while (run == true);
 
         }
 
@@ -27,10 +80,11 @@ public class Displayer
 	public void WriteLine(string message, int speed)
 	{
 		for (int i = 0; i < message.Length; i++)
-            {
+      {
                 Console.Write(message[i]);
                 System.Threading.Thread.Sleep(speed);
-            }
+      }
+      Console.WriteLine(); //add a new line at the end
 	}
 }
 
@@ -41,7 +95,7 @@ public class Storagehandler
 
 	public void ViewNotesChapters()
     {
-        Console.WriteLine("Listing notes:");
+        Console.WriteLine("Listing notes by chapter:");
 
         string[] noteFiles = Directory.GetFiles(".", "*.txt");
 
@@ -50,12 +104,30 @@ public class Storagehandler
             Console.WriteLine(Path.GetFileNameWithoutExtension(noteFile)); //Microsofts naming scheme is so vanilla it makes missionary look spicy
         }
 
-        Console.WriteLine("\nPress any key to continue...");
+        Console.WriteLine("\n");
+    }
+
+    public void ReadNote(string title)
+    {
+        string fileName = $"{title}.txt";
+
+        if (File.Exists(fileName))
+        {
+            string content = File.ReadAllText(fileName);  //		TODO: obscure it so notes app cant read it
+            Console.WriteLine($"\nNote Content:\n{content}");
+        }
+        else
+        {
+            Console.WriteLine($"Note '{title}' not found, check spelling.");
+        }
+
+        Console.WriteLine("\nPress any key to continue once you have finish reading");
         Console.ReadKey();
     }
+
 	public void CreateNote()	
     {
-        Console.Write("Enter note title: ");
+        Console.Write("Enter note chapter title: ");
         string title = Console.ReadLine();
 
 		Console.WriteLine("Enter your note content (enter an empty line without spaces to finish):");
@@ -72,11 +144,26 @@ public class Storagehandler
 
         string fileName = $"{title}.txt";  // $ indicated that string interpolation is being used, where {} is the indicator of a string being added, not text
 
-        File.WriteAllText(fileName, content);
+        File.WriteAllText(fileName, content);  //		TODO: obscure it so notes app cant read it
 
         Console.WriteLine("Note saved successfully!");
 		
-		
+    }
+
+    public void DeleteNote(string title)
+    {
+         string fileName = $"{title}.txt";
+
+        if (File.Exists(fileName))
+        {
+            File.Delete(fileName);
+            Console.WriteLine($"{fileName} is deleted now.");
+        }
+        else
+        {
+            Console.WriteLine($"Note '{title}' not found, check spelling.");
+        }
+
     }
 }
 }
