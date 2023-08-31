4a5fedf423b6d6adb600e77adf05d4c491d9823d
Alans Ciemgals <alan0648@mail.hansenberg.dk>
2023-08-31 08:55:14 +0200

    added stuff right before class

--- soft.cs
+++ soft.cs
@@ -6,6 +6,8 @@ class Testy
  static void Main(string[] args)
         {
                 bool run = true;
+                Storagehandler handl = new Storagehandler(); //honestly unnecesary, making it static would have been just fine as multiple instances wont ever be needed
+				Displayer display = new Displayer(); // but ig to show teacher that the student is able to do this and understand what it does ig
 
 
             Console.WriteLine("hello cock");
@@ -19,13 +21,49 @@ class Testy
         }
 
 }
-/* im still unsure how to dev this so i got a couple options
-1. simple terminal application as everyone else, maybe make fanciers with pages that clear out the screen every time and has a look trough pages thing with a list
-2. make a web app with .net so its nice and fancy
-3. make it so when launching you have the option whether to run on web or terminal
 
-Decided to make text based very simple first in abt a hour, if i got time and willpower will make a better version after.
 
-update: decided on 1, will have hardcoded notes perhaps, and additional ones you can make that get saved as txt files or smth in its own directory
-*/
+
+public class Storagehandler 
+{
+
+	public void ViewNotesChapters()
+    {
+        Console.WriteLine("Listing notes:");
+
+        string[] noteFiles = Directory.GetFiles(".", "*.txt");
+
+        foreach (string noteFile in noteFiles)
+        {
+            Console.WriteLine(Path.GetFileNameWithoutExtension(noteFile)); //Microsofts naming scheme is so vanilla it makes missionary look spicy
+        }
+
+        Console.WriteLine("\nPress any key to continue...");
+        Console.ReadKey();
+    }
+	public void CreateNote()	
+    {
+        Console.Write("Enter note title: ");
+        string title = Console.ReadLine();
+
+		Console.WriteLine("Enter your note content (enter an empty line without spaces to finish):");
+        
+        List<string> lines = new List<string>(); //probably a stupid way to go about this but whatever
+        string line;
+        
+        while ((line = Console.ReadLine()) != "") // is nice though becouse you can add a space so this doesnt get triggered, so you can make spacings ez
+        {
+            lines.Add(line);
+        }
+
+        string content = string.Join(Environment.NewLine, lines);
+
+        string fileName = $"{title}.txt";  // $ indicated that string interpolation is being used, where {} is the indicator of a string being added, not text
+
+        File.WriteAllText(fileName, content);
+
+        Console.WriteLine("Note saved successfully!");
+		
+		
+    }
 }
