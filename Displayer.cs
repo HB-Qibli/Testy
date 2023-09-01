cbff825c7cb1b65515e9fbec5f9cfbbc9cba422d
Alans Ciemgals <alan0648@mail.hansenberg.dk>
2023-09-01 07:56:55 +0200

    cleanup and readability

---
+++ Displayer.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace System
+{
+    
+public class Displayer
+{
+	public void WriteLine(string message, int speed, ConsoleColor color = ConsoleColor.White)
+	{
+        ConsoleColor originalColor = Console.ForegroundColor;
+
+        Console.ForegroundColor = color;
+
+		for (int i = 0; i < message.Length; i++)
+      {
+                Console.Write(message[i]);
+                System.Threading.Thread.Sleep(speed);
+      }
+      Console.ForegroundColor = originalColor;
+      Console.WriteLine(); //add a new line at the end
+	}
+    	// In future add more stuff for help to render or visualise this shit
+}
+}
